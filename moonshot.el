25ec5647f08f1bc7e2201b6b36c4e7d18e7ae3dd
Jong-Hyouk Yun <ageldama@gmail.com>
2019-12-29 03:42:02 +0900

    ...

--- moonshot.el
+++ moonshot.el
@@ -105,7 +105,8 @@ Can be a string or a form."
 (defcustom moonshot:runners-preset
   '("cmake -S\"%p\" -B\"%b\" -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=on"
     "cd \"%b\"; ninja"
-    "source \"%b/bin/activate\"; cd \"%p\"; \"%a\""
+    "source \"%b/bin/activate\"; cd \"%p\"; \"%a\"  # Run with Virtualenv"
+    "cd \"%p\"; \"%a\"  # Run script"
     )
   "Available shell command presets."
   :group 'moonshot
